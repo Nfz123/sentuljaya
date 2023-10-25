bf7e9205775908b454579e7517064c42c033d1c1
Ahmadhol <ahmadholidinpld@gmail.com>
2023-10-25 12:20:50 +0700

    index rt rw

--- resources/views/layouts/siode-front/rtrwfront/index.blade.php
+++ resources/views/layouts/siode-front/rtrwfront/index.blade.php
@@ -4,7 +4,7 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">
-                <P>PEMERINTAH DESA</P>
+                <P>RT RW DESA SENTUL JAYA</P>
             </h3>
             {{-- <div class="card-tools">
             <div class="input-group input-group-sm" style="width: 250px;">
